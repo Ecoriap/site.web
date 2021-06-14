e107a3cd9daca7ab31a29579c0aab22b0e9e14a7
Ecoriap <doctorborken@gmail.com>
2021-06-14 20:26:00 +0200

    Add files via upload

---
+++ updater.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+for (( ; ; ))
+do
+  echo "Supression du dossier site.web"
+  sudo rm -R site.web
+  echo "Clonage du dossier site.web"
+  git clone https://github.com/Ecoriap/site.web
+  clear
+  echo "[                              ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[/                             ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[//                            ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[///                           ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[////                          ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[/////                         ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[//////                        ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[///////                       ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[////////                      ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[/////////                     ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[//////////                    ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[///////////                   ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[////////////                  ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[/////////////                 ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[//////////////                ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[///////////////               ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[////////////////              ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[/////////////////             ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[//////////////////            ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[///////////////////           ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[////////////////////          ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[/////////////////////         ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[//////////////////////        ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[///////////////////////       ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[////////////////////////      ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[/////////////////////////     ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[//////////////////////////    ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[///////////////////////////   ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[////////////////////////////  ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[///////////////////////////// ]"
+  read -t 2
+  clear
+  echo "Et on va recommencer"
+  echo "[//////////////////////////////]"
+  read -t 2
+
+
+done
